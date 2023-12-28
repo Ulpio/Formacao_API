e438f46531380ae484aa79d4029c24ccc74b7728
Úlpio Netto <ulpionetto0@gmail.com>
2023-12-28 05:30:00 -0300

    crud

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,5 +5,7 @@ const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
 routes.post("/livros", LivroController.cadastraLivro);
+routes.put("/livros/:id", LivroController.atualizarLivro);
+routes.delete("/livros/:id", LivroController.deletarLivro);
 
 export default routes;
